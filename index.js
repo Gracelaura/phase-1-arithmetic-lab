d60eb61010f370c075f09e0582f1d6b90a9d9ac1
Gracelaura <gracenjuguna05@gmail.com>
2022-08-17 14:47:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
+let random = Math.floor(Math.random() * 10);
+
+let num4 = 34
+let num3 = 15
+let mod = num4 % num3
+
+let a = 10
+let b = 20
+let c = 5
+let max = Math.max(a,b,c)
 // Write your code here
